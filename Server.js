c120cc17745aea2cd0baa9b8f4cd3c2522100ae4
Debashis Kar Suvra <kar.suvra@gmail.com>
2023-04-29 22:01:20 +0600

    a

--- Server.js
+++ Server.js
@@ -14,9 +14,9 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.get('/', (req, res) => {
-    res.send('<html><body><h1>Hello, world!</h1></body></html>');
-  });
-  
+    res.sendFile(__dirname + '/index.html');
+});
+
 
 app.get('/states', (req, res) => {
     const isContig = req.query.contig === 'true';
@@ -266,7 +266,7 @@ app.delete('/states/:state/funfact', async (req, res) => {
 
 // catch-all route for 404 errors
 app.get('*', (req, res) => {
-    res.status(404).sendFile(__dirname + '/404.html');
+    res.status(500).sendFile(__dirname + '/404.html');
 });
 
 // app.get('*', (req, res) => {
